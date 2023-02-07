2320c0637d5d9cefad215a6b5c0e86d6bb54ad49
Your Name <toanntq@devcamp.edu.vn>
2023-02-07 12:51:14 +0700

    update create orderdetail

--- app/controllers/orderDetailController.js
+++ app/controllers/orderDetailController.js
@@ -175,95 +175,129 @@ const deleteOrderDetailById = (request, response) => {
     })
 }
 const getAllOrderDetailOfOrder = (request, response) => {
-     // B1: Chuẩn bị dữ liệu
-     const orderId = request.params.orderId;
-
-     // B2: Validate dữ liệu
-     if (!mongoose.Types.ObjectId.isValid(orderId)) {
-         return response.status(400).json({
-             status: "Bad Request",
-             message: "Course Id không hợp lệ"
-         })
-     }
- 
-     // B3: Thao tác với cơ sở dữ liệu
-     orderModel.findById(orderId)
-         .populate("orderDetail")
-         .exec((error, data) => {
-             if (error) {
-                 return response.status(500).json({
-                     status: "Internal server error",
-                     message: error.message
-                 })
-             }
- 
-             return response.status(200).json({
-                 status: "Get all detailOrder of order successfully",
-                 data: data
-             })
-         })
-}
-
-const createOrderDetailOfOrder = (request, response) => {
     // B1: Chuẩn bị dữ liệu
     const orderId = request.params.orderId;
-    const body = request.body;
 
     // B2: Validate dữ liệu
     if (!mongoose.Types.ObjectId.isValid(orderId)) {
         return response.status(400).json({
             status: "Bad Request",
-            message: "Order Id không hợp lệ"
-        })
-    }
-
-    if (!mongoose.Types.ObjectId.isValid(body.product)) {
-        return response.status(400).json({
-            status: "Bad Request",
-            message: "Product không hợp lệ"
-        })
-    }
-
-    if (!(Number.isInteger(body.quantity) && body.quantity > 0)) {
-        return response.status(400).json({
-            status: "Bad Request",
-            message: "Quantity không hợp lệ"
+            message: "Course Id không hợp lệ"
         })
     }
 
     // B3: Thao tác với cơ sở dữ liệu
-    const orderDetail = {
-        _id: mongoose.Types.ObjectId(),
-        product: body.product,
-        quantity: body.quantity
-    }
-
-    orderDetailModel.create(orderDetail, (error, data) => {
-        if (error) {
-            return response.status(500).json({
-                status: "Internal server error",
-                message: error.message
-            })
-        }
-        // Thêm id của detailOrder mới vào mảng orderDetail của Order đã chọn
-        orderModel.findByIdAndUpdate(orderId, {
-            $push: {
-                orderDetail: data._id
-            }
-        }, (err, updatedOrder) => {
-            if (err) {
+    orderModel.findById(orderId)
+        .populate("orderDetail")
+        .exec((error, data) => {
+            if (error) {
                 return response.status(500).json({
                     status: "Internal server error",
-                    message: err.message
+                    message: error.message
                 })
             }
 
-            return response.status(201).json({
-                status: "Create Order Detail Successfully",
+            return response.status(200).json({
+                status: "Get all detailOrder of order successfully",
                 data: data
             })
         })
-    })
+}
+
+const createOrderDetailOfOrder = async (request, response) => {
+    // B1: Chuẩn bị dữ liệu
+    const orderId = request.params.orderId;
+    const { product, quantity } = request.body;
+
+    // B2: Validate dữ liệu
+    const { error } = validateOrderDetail(orderId, product, quantity);
+    if (error) {
+        return response.status(400).json({
+            status: "Bad Request",
+            message: error
+        })
+    }
+
+    // B3: Thao tác với cơ sở dữ liệu
+    const newOrderDetail = {
+        _id: mongoose.Types.ObjectId(),
+        product: product,
+        quantity:quantity
+    }
+    //B4: Tạo orderDetail V2
+    try {
+        // Create the order in the database
+        const createdOrderDetail = await orderDetailModel.create(newOrderDetail);
+
+        // Add the order ID to the customer's order list
+        await orderModel.findByIdAndUpdate(orderId, {
+            $push: {
+                orderDetails: createdOrderDetail._id
+            }
+        }, { new: true });
+
+        // Return success response
+        return response.status(201).json({
+            status: "Create Order Successfully",
+            data: createdOrderDetail,
+        });
+    } catch (err) {
+        return response.status(500).json({
+            status: "Internal server error",
+            message: err.message
+        });
+    }
+    // // B4 Tạo orderDetail V1
+    // orderDetailModel.create(orderDetail, (error, data) => {
+    //     if (error) {
+    //         return response.status(500).json({
+    //             status: "Internal server error",
+    //             message: error.message
+    //         })
+    //     }
+    //     // Thêm id của detailOrder mới vào mảng orderDetail của Order đã chọn
+    //     orderModel.findByIdAndUpdate(orderId, {
+    //         $push: {
+    //             orderDetail: data._id
+    //         }
+    //     }, (err, updatedOrder) => {
+    //         if (err) {
+    //             return response.status(500).json({
+    //                 status: "Internal server error",
+    //                 message: err.message
+    //             })
+    //         }
+
+    //         return response.status(201).json({
+    //             status: "Create Order Detail Successfully",
+    //             data: data
+    //         })
+    //     })
+    // })
+}
+
+
+const validateOrderDetail = (pramOrderId, paramProduct, paramQuantity) => {
+    const errors = {}
+    // Validate pramOrderId
+    if (!mongoose.Types.ObjectId.isValid(pramOrderId)) {
+        errors.orderId = 'Invalid order Id';
+    }
+
+    // Validate pramOrderId
+    if (!mongoose.Types.ObjectId.isValid(paramProduct)) {
+        errors.product = 'Invalid product id';
+    }
+
+    // Validate paramQuantity
+    if (!paramQuantity || typeof paramQuantity !== 'number' || paramQuantity < 0) {
+        errors.quantity = 'Invalid cost';
+    }
+
+    return {
+        //Kiểm tra có lỗi xảy ra nếu có trả về mảng lỗi, không lỗi trả về null
+        error: Object.keys(errors).length > 0 ? errors : null
+    };
 }
 module.exports = {
     getAllOrderDetail,

54f1c126c7220c68f7079f4f4e48e5e5da3acc21
Your Name <toanntq@devcamp.edu.vn>
2023-03-17 17:21:35 +0700

    clean customer

--- app/routers/customerRouter.js
+++ app/routers/customerRouter.js
@@ -6,11 +6,11 @@ const express = require("express");
 const router = express.Router();
 
 // Import customer middleware
-const customerMiddleware = require("../middlewares/customerMiddleware");
 const userController = require("../controllers/userController");
 const userMiddleware = require("../middlewares/userMiddleware");
 // Import course controller
 const customerController = require("../controllers/customerController")
+const authMiddlewares = require("../middlewares/authMiddleware")
 
 router.get("/customers",
     // userMiddleware.authenticateUser,
@@ -18,27 +18,29 @@ router.get("/customers",
     customerController.getAllCustomer)
 
 router.post("/customers",
-    customerMiddleware.createCustomerMiddleware,
     // userMiddleware.authenticateUser,
     // userMiddleware.authorizeUser(['manager']),
     customerController.createCustomer)
 
 router.get("/customers/:customerId",
-    // customerMiddleware.getCustomerMiddleware,
     // userMiddleware.authenticateUser,
     // userMiddleware.authorizeUser(['manager']),
     customerController.getCustomerById)
 
 router.put("/customers/:customerId",
-    customerMiddleware.updateCustomerMiddleware,
     userMiddleware.authenticateUser,
     userMiddleware.authorizeUser(['manager']),
     customerController.updateCustomerById)
 
 router.delete("/customers/:customerId",
-    customerMiddleware.deleteCustomerMiddleware,
     userMiddleware.authenticateUser,
     userMiddleware.authorizeUser(['manager']),
     customerController.deleteCustomerById)
+
+
+
+router.get("/auth",
+    authMiddlewares.authFireBase,
+    customerController.getCustomerByEmail)
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-// Khai báo thư viện Express
-const express = require("express");
-const cors = require("cors")
-const cookieParser = require('cookie-parser')
-// khai báo mongoose 
-var mongoose = require('mongoose');
-const dotenv = require('dotenv');
-// Khai báo router app
-const productRouter = require("./app/routers/productRouter");
-const customerRouter = require("./app/routers/customerRouter");
-const orderRouter = require("./app/routers/orderRouter");
-const orderDetailRouter = require("./app/routers/orderDetailRouter");
-const userRouter = require("./app/routers/userRouter");
-const authRouter =require("./app/routers/authRouter")
+  // Khai báo thư viện Express
+  const express = require("express");
+  const cors = require("cors")
+  const cookieParser = require('cookie-parser')
+  // khai báo mongoose 
+  var mongoose = require('mongoose');
+  const dotenv = require('dotenv');
 
-dotenv.config();
+  //Set giá trị mongosee
+  // mongoose.set('strictQuery', false);
+  // Khai báo router app
+  const productRouter = require("./app/routers/productRouter");
+  const customerRouter = require("./app/routers/customerRouter");
+  const orderRouter = require("./app/routers/orderRouter");
+  const orderDetailRouter = require("./app/routers/orderDetailRouter");
+  const userRouter = require("./app/routers/userRouter");
+  // const authRouter =require("./app/routers/authRouter")
 
-// Khởi tạo Express App
-const app = express();
-//Khai báo port sử dụng
-const port = process.env.PORT || 8000;
+  dotenv.config();
 
-// Cấu hình request đọc được body json
-app.use(express.json());
-app.use(cors())
-app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-// Cấu hình request đọc được cookies
-app.use(cookieParser())
-// Khai báo để dử dụng UTF8
-app.use(express.urlencoded({ extended: true }))
+  // Khởi tạo Express App
+  const app = express();
+  //Khai báo port sử dụng
+  const port = process.env.PORT || 8000;
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Successfully MongoDB connected');
-  }
-});
+  // Cấu hình request đọc được body json
+  app.use(express.json());
+  app.use(cors())
+  app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
+  // Cấu hình request đọc được cookies
+  app.use(cookieParser())
+  // Khai báo để dử dụng UTF8
+  app.use(express.urlencoded({ extended: true }))
 
-// App sử dụng router
-//Express cho phép tối đa 5 middleware được thực hiện trong mỗi request. nên nếu có Middleware thứ 6 sẽ bị lỗ
-app.use("/", productRouter);
-app.use("/", authRouter);
-app.use("/", orderDetailRouter);
-app.use("/", customerRouter);
-app.use("/", userRouter);
-app.use("/", orderRouter);
+  mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Successfully MongoDB connected');
+    }
+  });
+
+  // App sử dụng router
+  //Express cho phép tối đa 5 middleware được thực hiện trong mỗi request. nên nếu có Middleware thứ 6 sẽ bị lỗ
+  app.use("/", productRouter);
+  // app.use("/", authRouter);
+  app.use("/", orderDetailRouter);
+  app.use("/", customerRouter);
+  app.use("/", userRouter);
+  app.use("/", orderRouter);
 
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
 
 
